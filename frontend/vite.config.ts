eedef82f750083e8c67edd3da83fce6c1ce63e72
Maksim Mikhailov <max@mer.pw>
2023-08-18 14:01:03 +0300

    Add Docker
    Add Dockerfiles for both frontend and backend
    
    Add Docker compose for container orchestration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,29 +1,37 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import ssr from "vite-plugin-ssr/plugin";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    ssr({
-      prerender: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      "#": "/src",
-    },
-  },
-  server: {
-    proxy: {
-      "/ws": {
-        target: "ws://localhost:8080",
-        ws: true,
-      },
-      "/api": {
-        target: "http://localhost:8080",
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const BACKEND_REWRITE_URL =
+    process.env.BACKEND_REWRITE_URL || "http://localhost:8080";
+  const BACKEND_WS_REWRITE_URL =
+    process.env.BACKEND_WS_REWRITE_URL || "ws://localhost:8080";
+
+  return defineConfig({
+    plugins: [
+      react(),
+      ssr({
+        prerender: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        "#": "/src",
       },
     },
-  },
-});
+    server: {
+      proxy: {
+        "/ws": {
+          target: BACKEND_WS_REWRITE_URL,
+          ws: true,
+        },
+        "/api": {
+          target: BACKEND_REWRITE_URL,
+        },
+      },
+    },
+  });
+};
