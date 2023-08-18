8efeee2f1a7aafddc2ba206e4bf9c36cdec6fd51
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-08-18 21:11:55 +0300

    add custom color presets

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,25 @@ import daisyUI from "daisyui";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#9463f4",
+          "secondary": "#dddcdc",
+          "accent": "#ffff00",
+          "neutral": "#7c7c7c",
+          "success": "#00ffd2",
+          "error": "#ff004f",
+            "info": "#00e8ff",
+            "warning": "#ffab00",
+          "base-100": "#292929",
+        },
+      },
+    ],
+  },
+
   theme: {
     extend: {
       fontFamily: {
