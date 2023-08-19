37fa75e51193fd3a6be40444168d55def7864681
Maksim Mikhailov <max@mer.pw>
2023-08-19 23:32:53 +0300

    Improve layout
    Make game field bigger
    
    Handle small screens

--- frontend/src/layouts/Global.tsx
+++ frontend/src/layouts/Global.tsx
@@ -4,7 +4,11 @@ import { Provider } from "react-redux";
 
 const GlobalLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div className={"container mx-auto flex h-full p-10 bg-base-300"}>
+    <div
+      className={
+        "container mx-auto h-screen flex flex-col md:flex-row p-10 bg-base-300"
+      }
+    >
       <Provider store={store}>{children}</Provider>
     </div>
   );
