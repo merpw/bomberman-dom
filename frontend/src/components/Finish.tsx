4cd451aa3f3fce5bdcaa3e715e4551b25d3be794
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-08-20 04:20:34 +0300

    add BIM and FAIL on finish

--- frontend/src/components/Finish.tsx
+++ frontend/src/components/Finish.tsx
@@ -19,7 +19,13 @@ const Finish = () => {
 
   return (
     <div className={"flex flex-col items-center justify-center h-full"}>
-      <h1 className={"text-[17rem] font-ibm_mono text-success my-11"}>BIM!</h1>
+      {username === winner.name ? (
+        <h1 className={"text-[17rem] font-ibm_mono text-success my-11"}>
+          BIM!
+        </h1>
+      ) : (
+        <h1 className={"text-[17rem] font-ibm_mono text-error my-11"}>FAIL</h1>
+      )}
       <p className={"text-lg"}>
         <span style={{ color }}>
           {username === winner.name ? "You" : winner.name}{" "}
